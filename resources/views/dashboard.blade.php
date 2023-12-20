40ed07c9384a84785e1669f46a6ec5ead1eec2cf
chronoxxlol <zen.joel1234@gmail.com>
2023-12-20 14:01:56 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,49 +1,4 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Student Information System</title>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            background-color: #f4f4f4;
-        }
-
-        .container {
-            text-align: center;
-        }
-
-        h1 {
-            color: #333;
-        }
-
-        p {
-            color: #666;
-        }
-
-        a {
-            text-decoration: none;
-            color: #3490dc;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h1>Welcome to Student Information System</h1>
-        <p>This is a simple web application to manage student information.</p>
-        <p>Get started by <a href="{{ route('students.index') }}">viewing the student list</a> or <a href="{{ route('students.create') }}">adding a new student</a>.</p>
-    </div>
-</body>
-</html> --}}
-
-{{-- <!doctype html>
+<!doctype html>
 <html lang="en">
     <style>
         body {
@@ -62,7 +17,7 @@
         }
     </style>
     <head>
-        <title>Dashboard</title>
+        <title>Welcome</title>
         <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta
@@ -114,4 +69,4 @@
             crossorigin="anonymous"
         ></script>
     </body>
-</html> --}}
+</html>
